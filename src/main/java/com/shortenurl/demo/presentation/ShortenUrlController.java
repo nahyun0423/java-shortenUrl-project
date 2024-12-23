9b8e37d274174e539a2a90e11abfbdcb0c3571aa
nahyun0423 <skgus0423@naver.com>
2024-12-23 17:05:10 +0900

    [feat] controller 추가

--- src/main/java/com/shortenurl/demo/presentation/ShortenUrlController.java
+++ src/main/java/com/shortenurl/demo/presentation/ShortenUrlController.java
@@ -1,5 +1,17 @@
 package com.shortenurl.demo.presentation;
 
+import com.shortenurl.demo.domain.ShortenUrl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ShortenUrlController {
+
+    @GetMapping(value = "/shortenUrl")
+    public ShortenUrlDto createShortenUrl(@RequestBody String originalUrl) {
+        ShortenUrl shortenUrl = new ShortenUrl(originalUrl);
+        return ShortenUrlDto.toDto(shortenUrl);
+    }
 
 }
