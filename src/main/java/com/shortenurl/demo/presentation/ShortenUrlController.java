e612d753cf8cf064adb328cb5f35464fad1aaf57
nahyun0423 <skgus0423@naver.com>
2025-01-03 16:56:50 +0900

    [chore] 컨트롤러 리턴값 수정

--- src/main/java/com/shortenurl/demo/presentation/ShortenUrlController.java
+++ src/main/java/com/shortenurl/demo/presentation/ShortenUrlController.java
@@ -3,21 +3,25 @@ package com.shortenurl.demo.presentation;
 import com.shortenurl.demo.application.ShortenUrlService;
 import com.shortenurl.demo.domain.ShortenUrl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class ShortenUrlController {
     private final ShortenUrlService shortenUrlService;
 
-    @GetMapping(value = "/shortenUrl")
-    public ShortenUrlDto createShortenUrl(@RequestBody String originalUrl) {
+    @PostMapping(value = "/shortenUrl")
+    public ResponseEntity<ShortenUrlDto> createShortenUrl(@RequestBody Map<String, String> requestBody) {
+        String originalUrl = requestBody.get("originalUrl");
         String newShortenUrl = shortenUrlService.createShortenUrl(originalUrl);
         ShortenUrl shortenUrl = new ShortenUrl(originalUrl, newShortenUrl);
         shortenUrlService.saveShortenUrl(shortenUrl);
-        return new ShortenUrlDto(shortenUrl);
+        return ResponseEntity.ok(new ShortenUrlDto(shortenUrl));
     }
 
 }
