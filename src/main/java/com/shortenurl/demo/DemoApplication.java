0d0e5effa58a3bc6bd25a7131e60d9ff59c544e1
nahyun0423 <skgus0423@naver.com>
2025-01-11 02:33:49 +0900

    test commit

--- src/main/java/com/shortenurl/demo/DemoApplication.java
+++ src/main/java/com/shortenurl/demo/DemoApplication.java
@@ -2,15 +2,11 @@ package com.shortenurl.demo;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
-@SpringBootApplication
-@EntityScan(basePackages = "com.shortenurl.demo.domain")
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/shortenurl/demo/domain/ShortKeyGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.shortenurl.demo.domain;
-
-import java.util.Objects;
-
-public interface ShortKeyGenerator {
-    String generateKey(String originalUrl);
-}
